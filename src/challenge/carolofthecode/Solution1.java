3f8907c13f7f9236030bc4d44dcc77104c8b0e22
nicholas <miaodi1985@126.com>
2023-01-20 15:02:52 +0800

    a new solution

--- src/challenge/carolofthecode/Solution1.java
+++ src/challenge/carolofthecode/Solution1.java
@@ -4,7 +4,7 @@ package challenge.carolofthecode;
 public class Solution1 {
     public int solution(String[] A) {
         if(A.length == 1) return 0;
-        int startIndex = 0, curIndex = 0, maxLen = 0, curLen = 0;
+        int startIndex = 0, curIndex = 0, maxLen = 1, curLen = 1;
         for(int i = 1; i < A.length; i++){
             if(A[i-1].charAt(1) == A[i].charAt(3)){
                 if(maxLen < ++curLen){
@@ -13,18 +13,47 @@ public class Solution1 {
                 }
             }else{
                 curIndex = i;
-                curLen = 0;
+                curLen = 1;
             }
         }
-        int result = 0;
-        for(int i = startIndex - 1; i > -1; i--){
-            if(A[i].charAt(1) == A[i+1].charAt(3)) continue;
-            result += A[i].charAt(3) == A[i+1].charAt(3) ? 2 : 1;
+        int result = 0, oppositeChar;
+        if(startIndex - 1 > -1){
+            oppositeChar = A[startIndex].charAt(3);
+            for(int i = startIndex - 1; i > -1; i--){
+                if(A[i].charAt(1) == oppositeChar) {
+                    oppositeChar = A[i].charAt(3);
+                    continue;
+                }
+                if(A[i].charAt(3) == oppositeChar){
+                    result += 2;
+                    oppositeChar = A[i].charAt(1);
+                }else{
+                    result += 1;
+                    oppositeChar = A[i].charAt(0) == oppositeChar ? A[i].charAt(2) : A[i].charAt(0);
+                }
+            }
         }
-        for(int i = startIndex + maxLen; i < A.length; i++){
-            if(A[i-1].charAt(1) == A[i].charAt(3)) continue;
-            result += A[i-1].charAt(3) == A[i].charAt(3) ? 2 : 1;
+        if(startIndex + maxLen < A.length){
+            oppositeChar = A[startIndex + maxLen - 1].charAt(1);
+            for(int i = startIndex + maxLen; i < A.length; i++){
+                if(oppositeChar == A[i].charAt(3)) {
+                    oppositeChar = A[i].charAt(1);
+                    continue;
+                }
+                if(oppositeChar == A[i].charAt(1)){
+                    result += 2;
+                    oppositeChar = A[i].charAt(3);
+                }else{
+                    result += 1;
+                    oppositeChar = A[i].charAt(0) == oppositeChar ? A[i].charAt(2) : A[i].charAt(0);
+                }
+            }
         }
         return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Solution1().solution(new String[]{"0123", "3120", "2301"}));
+
     }
 }

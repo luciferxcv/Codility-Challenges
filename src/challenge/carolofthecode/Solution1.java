10251aef08c4062fd9e6a5a5a5e652d973597fcd
nicholas <miaodi1985@126.com>
2023-01-28 17:16:00 +0800

    a new solution

--- src/challenge/carolofthecode/Solution1.java
+++ src/challenge/carolofthecode/Solution1.java
@@ -3,57 +3,41 @@ package challenge.carolofthecode;
 //TODO: unfinished
 public class Solution1 {
     public int solution(String[] A) {
-        if(A.length == 1) return 0;
-        int startIndex = 0, curIndex = 0, maxLen = 1, curLen = 1;
-        for(int i = 1; i < A.length; i++){
-            if(A[i-1].charAt(1) == A[i].charAt(3)){
-                if(maxLen < ++curLen){
-                    maxLen = curLen;
-                    startIndex = curIndex;
+        int len = A.length;
+        if(len == 1) return 0;
+        int leftBased = 0, rightBased = 0;
+        int leftJoint = A[0].charAt(1), rightJoint = A[len-1].charAt(3);
+        for(int i = 1, j = len - 2; i < len; i++, j--){
+            // left one based, rotate right one
+            if(A[i].charAt(3) != leftJoint){
+                if(A[i].charAt(1) == leftJoint){
+                    leftBased += 2;
+                    leftJoint = A[i].charAt(1);
+                }else{
+                    leftBased += 1;
+                    leftJoint = A[i].charAt(0) == leftJoint ? A[i].charAt(2) : A[i].charAt(0);
                 }
             }else{
-                curIndex = i;
-                curLen = 1;
+                leftJoint = A[i].charAt(1);
             }
-        }
-        int result = 0, oppositeChar;
-        if(startIndex - 1 > -1){
-            oppositeChar = A[startIndex].charAt(3);
-            for(int i = startIndex - 1; i > -1; i--){
-                if(A[i].charAt(1) == oppositeChar) {
-                    oppositeChar = A[i].charAt(3);
-                    continue;
-                }
-                if(A[i].charAt(3) == oppositeChar){
-                    result += 2;
-                    oppositeChar = A[i].charAt(1);
+            // right one based, rotate left one
+            if(A[j].charAt(1) != rightJoint){
+                if(A[j].charAt(3) == rightJoint){
+                    rightBased += 2;
+                    rightJoint = A[j].charAt(1);
                 }else{
-                    result += 1;
-                    oppositeChar = A[i].charAt(0) == oppositeChar ? A[i].charAt(2) : A[i].charAt(0);
+                    rightBased += 1;
+                    rightJoint = A[j].charAt(0) == rightJoint ? A[j].charAt(2) : A[j].charAt(0);
                 }
+            }else{
+                rightJoint = A[j].charAt(3);
             }
         }
-        if(startIndex + maxLen < A.length){
-            oppositeChar = A[startIndex + maxLen - 1].charAt(1);
-            for(int i = startIndex + maxLen; i < A.length; i++){
-                if(oppositeChar == A[i].charAt(3)) {
-                    oppositeChar = A[i].charAt(1);
-                    continue;
-                }
-                if(oppositeChar == A[i].charAt(1)){
-                    result += 2;
-                    oppositeChar = A[i].charAt(3);
-                }else{
-                    result += 1;
-                    oppositeChar = A[i].charAt(0) == oppositeChar ? A[i].charAt(2) : A[i].charAt(0);
-                }
-            }
-        }
-        return result;
+        return Math.min(leftBased, rightBased);
     }
 
     public static void main(String[] args){
-        System.out.println(new Solution1().solution(new String[]{"0123", "3120", "2301"}));
+        System.out.println(new Solution1().solution(new String[]{"rbgw","gbrw", "rwgb", "gbrw"}));
 
     }
 }

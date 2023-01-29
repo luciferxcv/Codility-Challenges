6ad70d738dcc8834a0ce1e457c3ca78f5f951bdb
nicholas <miaodi1985@126.com>
2023-01-29 10:19:15 +0800

    a new solution

---
+++ src/challenge/yearoftherabbit/Solution1.java
@@ -0,0 +1,16 @@
+package challenge.yearoftherabbit;
+
+public class Solution1 {
+    public int Solution(int[] A, int[] B){
+        int len = A.length, rotateTimes = 0;
+        for(int i = 0; i < len; i++){
+            if(rotateTimes == len) return -1;
+            int bIndex = (i - rotateTimes + len) % len;
+            if(A[i] == B[bIndex]){
+                rotateTimes++;
+                i = -1;
+            }
+        }
+        return rotateTimes;
+    }
+}

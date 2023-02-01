c8c8d95ae55fc3594bdb599fe54efb262912186c
nicholas <miaodi1985@126.com>
2023-02-01 10:59:13 +0800

    provide a new solution to Lesson5-CountDiv of codility lessons

--- src/lessons/lesson5/CountDiv.java
+++ src/lessons/lesson5/CountDiv.java
@@ -1,15 +1,13 @@
 package lessons.lesson5;
 
 public class CountDiv {
-    //TODO: unfinished
     public int solution(int A, int B, int K){
-        if(K > B) return 0;
-        if(K == 1) return B - A + 1;
-        int next = A % K == 0 ? A : K - (A % K) + A;
-        return (int)Math.ceil((B - next)*1.0/K);
+        if(A % K == 0)
+            return (B - A) / K + 1;
+        return (B - A + A % K) / K;
     }
 
     public static void main(String[] args){
-        System.out.println(new CountDiv().solution(100, 123_000_000, 2));
+        System.out.println(new CountDiv().solution(0, Integer.MAX_VALUE, 2));
     }
 }

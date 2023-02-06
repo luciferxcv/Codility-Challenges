21763b6617cda5578eab0757f1e66fe4bdf45e95
nicholas <miaodi1985@126.com>
2023-02-06 13:15:44 +0800

    optimize

--- src/lessons/lesson8/Dominator.java
+++ src/lessons/lesson8/Dominator.java
@@ -6,26 +6,28 @@ import java.util.Map;
 public class Dominator {
     public int solution(int[] A){
         int len = A.length;
-        if(len == 0) return -1;
-        int max = 1;
-        int index = 0;
+        if(len == 1) return 0;
+        int target = len / 2;
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < len; i++){
             if(map.containsKey(A[i])){
                 int count = map.get(A[i]) + 1;
-                map.put(A[i], count);
-                if(count > max){
-                    max = count;
-                    index = i;
-                }
+                if(count > target)
+                    return i;
+                else
+                    map.put(A[i], count);
             }else{
                 map.put(A[i], 1);
             }
         }
-        return max > len / 2 ? index : -1;
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Dominator().solution(new int[]{2,2,2,1,1,1,1,1,1,2,2,2}));
+        Dominator dominator = new Dominator();
+        System.out.println(dominator.solution(new int[]{2,2,2,1,1,1,1,1,1,2,2,2}));
+        System.out.println(dominator.solution(new int[]{}));
+        System.out.println(dominator.solution(new int[]{1}));
+        System.out.println(dominator.solution(new int[]{1,1,2,2,2}));
     }
 }
